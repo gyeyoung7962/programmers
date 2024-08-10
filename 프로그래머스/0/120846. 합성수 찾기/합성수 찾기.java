e3e61fd003105190394b82838bef156c290a877b
gaeyoung7962 <49228577+gyeyoung7962@users.noreply.github.com>
2024-08-10 14:24:21 +0900

    [level 0] Title: 합성수 찾기, Time: 0.09 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int divisorCount = 0;
+
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    divisorCount++;
+                }
+            }
+
+            if (divisorCount >= 3) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
