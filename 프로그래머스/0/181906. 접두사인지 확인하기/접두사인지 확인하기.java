5fed5c87104195c81c35b126237f449f51d692cf
gaeyoung7962 <49228577+gyeyoung7962@users.noreply.github.com>
2024-09-15 22:13:55 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        
+        
+        int answer = 0;
+
+        if (my_string.length() > is_prefix.length() && my_string.startsWith(is_prefix)) {
+            answer = 1;
+        }
+
+        return answer;
+    }
+}
