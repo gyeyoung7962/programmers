793b9355ca89825c9d60b0a524f4343c83c5806c
gaeyoung7962 <49228577+gyeyoung7962@users.noreply.github.com>
2024-10-03 20:43:36 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        answer = String.valueOf(Integer.parseInt(n_str));
+        
+        
+        return answer;
+    }
+}
