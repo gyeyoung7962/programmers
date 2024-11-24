ac040ccaa6a4a7df6799ab969b2f23f4a4ff7e10
gaeyoung7962 <49228577+gyeyoung7962@users.noreply.github.com>
2024-11-24 23:23:23 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.21 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        
+        String[] arr = my_string.split(" ");
+
+        answer = new String[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            answer[i] = arr[i];
+        }
+        return answer;
+    }
+}
