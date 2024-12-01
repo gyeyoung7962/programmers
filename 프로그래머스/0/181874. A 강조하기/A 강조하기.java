bf2fcd077cbf8ec946f70fd78453ca66e74443d2
gaeyoung7962 <49228577+gyeyoung7962@users.noreply.github.com>
2024-12-01 19:30:36 +0900

    [level 0] Title: A 강조하기, Time: 0.09 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String myString) {
+        
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < myString.length(); i++) {
+            
+            char ch = myString.charAt(i);
+
+            if (ch == 'a') {
+                sb.append(Character.toUpperCase(ch)); 
+            }
+            else if (ch == 'A') {
+                sb.append('A');
+            }
+            else if (Character.isUpperCase(ch)) {
+                sb.append(Character.toLowerCase(ch)); 
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
